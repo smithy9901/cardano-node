a5c4b784c9cd40cf6141aa126aec2e9ff9c1a59d
Yuriy Syrovetskiy <yuriy.syrovetskiy@iohk.io>
2021-12-10 18:52:24 +0300

    cardano-api:gen: Fix hash collisions in TxOut script data

--- cardano-api/gen/Gen/Cardano/Api/Typed.hs
+++ cardano-api/gen/Gen/Cardano/Api/Typed.hs
@@ -55,6 +55,18 @@ module Gen.Cardano.Api.Typed
   , genRational
   ) where
 
+import           Cardano.Prelude
+
+import           Control.Monad.Fail (fail)
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Short as SBS
+import           Data.Coerce
+import qualified Data.Map.Strict as Map
+import           Data.String
+import           Hedgehog (Gen, Range)
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+
 import           Cardano.Api hiding (txIns)
 import qualified Cardano.Api as Api
 import           Cardano.Api.Byron (KeyWitness (ByronKeyWitness),
@@ -64,23 +76,11 @@ import           Cardano.Api.Shelley (Hash (ScriptDataHash), KESPeriod (KESPerio
                    PlutusScript (PlutusScriptSerialised), ProtocolParameters (ProtocolParameters),
                    StakeCredential (StakeCredentialByKey), StakePoolKey)
 
-import           Cardano.Prelude
-
-import           Control.Monad.Fail (fail)
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Short as SBS
-import           Data.Coerce
-import           Data.String
-
 import qualified Cardano.Binary as CBOR
 import qualified Cardano.Crypto.Hash as Crypto
 import qualified Cardano.Crypto.Seed as Crypto
 import qualified Cardano.Ledger.Shelley.TxBody as Ledger (EraIndependentTxBody)
 import qualified Plutus.V1.Ledger.Api as Plutus
-
-import           Hedgehog (Gen, Range)
-import qualified Hedgehog.Gen as Gen
-import qualified Hedgehog.Range as Range
 
 import qualified Cardano.Crypto.Hash.Class as CRYPTO
 import           Cardano.Ledger.SafeHash (unsafeMakeSafeHash)
@@ -506,7 +506,11 @@ genTxBodyContent :: CardanoEra era -> Gen (TxBodyContent BuildTx era)
 genTxBodyContent era = do
   txIns <- map (, BuildTxWith (KeyWitness KeyWitnessForSpending)) <$> Gen.list (Range.constant 1 10) genTxIn
   txInsCollateral <- genTxInsCollateral era
-  txOuts <- Gen.list (Range.constant 1 10) (genTxOut era)
+  txOuts <-
+    Gen.list (Range.constant 1 10) (genTxOut era)
+    <&> fixDatumHashCollisions
+      -- Without this fix, generated script data may have the same hashes for
+      -- both present (value + hash) and non-present (hash only) values.
   txFee <- genTxFee era
   txValidityRange <- genTxValidityRange era
   txMetadata <- genTxMetadataInEra era
@@ -535,6 +539,33 @@ genTxBodyContent era = do
     , Api.txMintValue
     , Api.txScriptValidity
     }
+
+
+-- | Ensure that all script data with the same hash are
+-- either all presented as values or all presented as hashes.
+--
+-- It's possible to have a hash without its datum, and also with its datum,
+-- and these two possibilities are semantically equivalent.
+fixDatumHashCollisions :: forall era. [TxOut CtxTx era] -> [TxOut CtxTx era]
+fixDatumHashCollisions outs = map replaceOutHashWithItsDatum outs where
+
+  replaceOutHashWithItsDatum :: TxOut CtxTx era -> TxOut CtxTx era
+  replaceOutHashWithItsDatum (TxOut address value datum) =
+    TxOut address value (replaceHashWithItsDatum datum)
+
+  replaceHashWithItsDatum :: TxOutDatum CtxTx era -> TxOutDatum CtxTx era
+  replaceHashWithItsDatum datum =
+    case datum of
+      TxOutDatumHash _ hash -> fromMaybe datum $ Map.lookup hash hashedData
+      _ -> datum
+
+  hashedData :: Map (Hash ScriptData) (TxOutDatum CtxTx era)
+  hashedData =
+    Map.fromList
+      [ (hashScriptData scriptData, datum)
+      | TxOut _ _ datum@(TxOutDatum _ scriptData) <- outs
+      ]
+
 
 genTxInsCollateral :: CardanoEra era -> Gen (TxInsCollateral era)
 genTxInsCollateral era =
